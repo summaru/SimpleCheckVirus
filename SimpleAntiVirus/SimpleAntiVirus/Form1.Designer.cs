9e6a8f78f1d23e6d88ab3efed993ef2175df73de
summaru <dkstjd5183@gmail.com>
2017-05-20 00:35:21 +0900

    SimpleCheck

---
+++ SimpleAntiVirus/SimpleAntiVirus/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace SimpleAntiVirus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBrowes = new System.Windows.Forms.Button();
+            this.txtMD5 = new System.Windows.Forms.TextBox();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblMD5 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnBrowes
+            // 
+            this.btnBrowes.Location = new System.Drawing.Point(197, 33);
+            this.btnBrowes.Name = "btnBrowes";
+            this.btnBrowes.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowes.TabIndex = 0;
+            this.btnBrowes.Text = "Browes File";
+            this.btnBrowes.UseVisualStyleBackColor = true;
+            this.btnBrowes.Click += new System.EventHandler(this.btnBrowes_Click);
+            // 
+            // txtMD5
+            // 
+            this.txtMD5.Location = new System.Drawing.Point(54, 69);
+            this.txtMD5.Name = "txtMD5";
+            this.txtMD5.Size = new System.Drawing.Size(218, 21);
+            this.txtMD5.TabIndex = 1;
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(12, 35);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(179, 21);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(10, 105);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(63, 14);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Status :";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("돋움", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblTitle.Location = new System.Drawing.Point(62, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(162, 19);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "Simple Anti-Virus";
+            // 
+            // lblMD5
+            // 
+            this.lblMD5.AutoSize = true;
+            this.lblMD5.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMD5.Location = new System.Drawing.Point(12, 71);
+            this.lblMD5.Name = "lblMD5";
+            this.lblMD5.Size = new System.Drawing.Size(42, 14);
+            this.lblMD5.TabIndex = 5;
+            this.lblMD5.Text = "MD5 :";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(79, 105);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 14);
+            this.lblResult.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 160);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblMD5);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.txtMD5);
+            this.Controls.Add(this.btnBrowes);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SimpleAntiVirus";
+            this.Load += new System.EventHandler(this.SimpleAntiVirus_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowes;
+        private System.Windows.Forms.TextBox txtMD5;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblMD5;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ SimpleAntiVirus/SimpleAntiVirus/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace SimpleAntiVirus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ 
+
+        private void SimpleAntiVirus_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string md5_hash(string file_name)
+        {
+            return hash_generator("md5", file_name);
+        }
+
+        private string hash_generator(string hash_type,string file_name)
+        {
+            using (var hash = MD5.Create())
+            {
+                FileStream filestream = File.OpenRead(file_name);
+                filestream.Position = 0;
+                var hashValue = hash.ComputeHash(filestream);
+                string hash_hex = PrintByteArray(hashValue);
+
+                filestream.Close();
+                return hash_hex;
+            }
+        }
+
+        public string PrintByteArray(byte[] array)
+        {
+            string hex_value = "";
+            hex_value = string.Join("",array.Select(i => i.ToString("x2")));
+
+            return hex_value.ToLower();
+        }
+
+        private void btnBrowes_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                txtFilePath.Text = path;
+
+                string sample = md5_hash(path);
+                txtMD5.Text = md5_hash(path);
+                string[] lines = File.ReadAllLines("0.md5");
+                foreach (var line in lines)
+                {
+                    if (line == sample)
+                    {
+                        lblResult.ForeColor = Color.Red;
+                        lblResult.Text = "infected!";
+                    }
+                    else
+                    {
+                        lblResult.ForeColor = Color.Green;
+                        lblResult.Text = "Clean";
+                    }
+
+                }
+                
+            }
+        }
+
+
+    }
+}
